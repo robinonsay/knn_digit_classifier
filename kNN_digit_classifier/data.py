0e2abbd11ae2c03d2bdb420aad1080f03368f45e
Robin Onsay <alper@robinonsay.me>
2022-01-17 07:56:06 -0500

    changes data import to Series. It doesn't need to be stored as an image

--- kNN_digit_classifier/data.py
+++ kNN_digit_classifier/data.py
@@ -9,8 +9,7 @@ def import_training_data(training_filename: str) -> Dict:
     training_df = pd.read_csv(training_filename)
     for _, img_series in training_df.iterrows():
         key = img_series.get("label")
-        img_1d = img_series.drop("label").to_numpy()
-        img = numpy.reshape(img_1d, (const.IMAGE_HEIGHT, const.IMAGE_WIDTH))
+        img = img_series.drop("label").to_numpy()
         if key in training_data:
             training_data[key].append(img)
         else:
@@ -22,6 +21,5 @@ def import_test_data(test_filename: str) -> List:
     training_data = []
     training_df = pd.read_csv(test_filename)
     for _, img_series in training_df.iterrows():
-        img = numpy.reshape(img_series.to_numpy(), (const.IMAGE_HEIGHT, const.IMAGE_WIDTH))
-        training_data.append(img)
+        training_data.append(img_series.to_numpy())
     return training_data
